b9792a97746929c1b954491e8477d899e78f6212
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-22 15:12:44 -0500

    open to hoonings page

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n audi
 sfdx automig:load -d data
-sfdx force:org:open
+sfdx force:org:open -p /lightning/o/Hooning__c/list?filterName=All
